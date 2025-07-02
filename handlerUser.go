eafcd10e26a4d099bf9023d12346c764c4dd6887
Arish Imam <arish.imam@gmail.com>
2025-07-01 21:35:01 -0700

    db in progress

--- handlerUser.go
+++ handlerUser.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/arishimam/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -17,5 +22,34 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("user has been set to ", user)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("argument slice is empty")
+	}
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("no name was passed in args!")
+	}
+
+	userName := cmd.args[0]
+
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      userName,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return err
+	}
+
+	s.cfg.SetUser(userName)
+	fmt.Println("user has been created and set to: ", userName)
+	fmt.Println("user: ", user)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/arishimam/gator/internal/config"
 	"log"
 	"os"
+
+	"github.com/arishimam/gator/internal/config"
+	"github.com/arishimam/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
 	config, err := config.Read()
+
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", config.DbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &config,
 	}
 
 	cmds := commands{make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		fmt.Println("No command-line arguments passed in")
